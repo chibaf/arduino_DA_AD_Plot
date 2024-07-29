8899339711d5dfc34fb7ec5f659ad31e97ab8ceb
chibaf <chibaf@mac.com>
2024-07-29 20:04:42 +0900

    Add files via upload

---
+++ ArduinoUNOR4_ADC14.ino
@@ -0,0 +1,10 @@
+void setup(){
+  analogReadResolution(14); //change to 14-bit resolution
+  Serial.begin(9600);
+}
+
+void loop(){
+  int reading = analogRead(A0); // returns a value between 0-16383
+  Serial.println(reading);
+  delayMicroseconds(10); // sleep 10 microseconds
+}
---
+++ waveplot.py
@@ -0,0 +1,19 @@
+import serial, sys
+import matplotlib.pyplot as plt
+
+x=range(0, 100, 1)
+data=[0.0]*100
+
+ser=serial.Serial(sys.argv[1],sys.argv[2])
+print("connected to: " + ser.portstr)
+while True:
+  line = ser.readline()
+  line1=line.strip().decode('utf-8')
+  d=float(line1)
+  print(d)
+  data.pop(-1)
+  data.insert(0,d)
+  plt.clf()
+  plt.ylim([-1.0,20000.0])
+  plt.plot(x,data)
+  plt.pause(0.1)
